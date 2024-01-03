30f307bf5c1616aeb248988f65e367821e7aabf9
Aaron B D'Souza <77206383+AaronBDSouza@users.noreply.github.com>
2024-01-03 23:18:05 +0530

    Fixed Broken Links in TopBar Component

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -38,24 +38,45 @@ export default function TopBar({menuOpen,setMenuOpen}) {
 
                     <div className="iconLinks">
                         <div className="itemContainer linkedin">
-                            <LinkedIn className="icon"/>
-                            <span className="iconLink"><a href="https://www.linkedin.com/in/aaron-baptista-d-souza?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BBrbv4ZdiTN6BLcbb1Rvp1Q%3D%3D">aaronbdsouza</a></span>
+                            <a href="https://www.linkedin.com/in/aaron-baptista-d-souza">
+                                <LinkedIn className="icon"/>
+                                <span className="iconLink">aaronbdsouza</span>
+                            </a>
+                            
+                                {/* <a href="https://www.linkedin.com/in/aaron-baptista-d-souza"></a> */}
+                            
                         </div>
                         <div className="itemContainer gmail">
-                            <Mail className="icon"/>
-                            <span className="iconLink"><a href="mailto:aaronbdsouza7@gmail.com">aaronbdsouza7@gmail.com</a></span>
+                            <a href="mailto:aaronbdsouza7@gmail.com">
+                                <Mail className="icon"/>
+                            </a>
+                            <span className="iconLink">
+                                <a href="mailto:aaronbdsouza7@gmail.com">aaronbdsouza7@gmail.com</a>
+                            </span>
                         </div>
                         <div className="itemContainer github">
-                            <GitHub className="icon"/>
-                            <span className="iconLink"><a href="https://github.com/aaronbdsouza">aaronbdsouza</a></span>
+                            <a href="https://github.com/aaronbdsouza">
+                                <GitHub className="icon"/>
+                            </a>
+                            <span className="iconLink">
+                                <a href="https://github.com/aaronbdsouza">aaronbdsouza</a>
+                            </span>
                         </div>
                         <div className="itemContainer instagram">
-                            <Instagram className="icon"/>
-                            <span className="iconLink"><a href="https://www.instagram.com/arrowknight007/">arrowknight007</a></span>
+                            <a href="https://www.instagram.com/arrowknight007/">
+                                <Instagram className="icon"/>
+                            </a>
+                            <span className="iconLink">
+                                <a href="https://www.instagram.com/arrowknight007/">arrowknight007</a>
+                            </span>
                         </div>
                         <div className="itemContainer cv">
-                            <Person className="icon"/>
-                            <span className="iconLink"><a href="assets/cv/CV-AaronBDSouza.pdf" download="CV - Aaron B D Souza">C.V</a></span>
+                            <a href="assets/cv/CV-AaronBDSouza.pdf" download="CV - Aaron B D Souza">
+                                <Person className="icon"/>
+                            </a>
+                            <span className="iconLink">
+                                <a href="assets/cv/CV-AaronBDSouza.pdf" download="CV - Aaron B D Souza">C.V</a>
+                            </span>
                         </div>
                     </div>
 
