b3abb2533d52545f2a48e108196a392411d3a0c1
Aaron B D'Souza <77206383+AaronBDSouza@users.noreply.github.com>
2024-01-03 22:03:44 +0530

    Updated Asset Files

--- src/galleryData.js
+++ src/galleryData.js
@@ -23,6 +23,34 @@ export const galleryData = [
     },
     {
         id: 4,
+        title: "Genesis",
+        location: "MES College, Vasco",  
+        year: "2017",         
+        img: "assets/gallery_images/genesis1.jpg"
+    },
+    {
+        id: 4,
+        title: "Genesis",
+        location: "MES College, Zuarinagar",  
+        year: "2017",         
+        img: "assets/gallery_images/genesis2.jpg"
+    },
+    {
+        id: 4,
+        title: "Genesis",
+        location: "MES College, Zuarinagar",  
+        year: "2017",         
+        img: "assets/gallery_images/genesis3.jpg"
+    },
+    {
+        id: 3,
+        title: "Colossus 7.0",
+        location: "Don Bosco College, Panjim",  
+        year: "2018",         
+        img: "assets/gallery_images/colossus4.jpg"
+    },
+    {
+        id: 4,
         title: "Crescendo",
         location: "St. Xavier's College, Mapusa",  
         year: "2018",         
@@ -44,17 +72,17 @@ export const galleryData = [
     },
     {
         id: 7,
-        title: "Techlipse - Organizing Team",
+        title: "Techlipse - Organizers",
         location: "St. Xavier's College, Mapusa",   
         year: "2018",          
-        img: "assets/gallery_images/techlipse1.jpg"
+        img: "assets/gallery_images/techlipse11.jpg"
     },
     {
         id: 8,
-        title: "Techlipse - Organizing Team",
+        title: "Techlipse - Organizers",
         location: "St. Xavier's College, Mapusa",   
         year: "2018",          
-        img: "assets/gallery_images/techlipse2.jpg"
+        img: "assets/gallery_images/techlipse12.jpg"
     },
     {
         id: 9,
@@ -62,6 +90,20 @@ export const galleryData = [
         location: "3D Systems Office, Porvorim,",   
         year: "2020",          
         img: "assets/gallery_images/3dsystems1.jpg"
+    },
+    {
+        id: 9,
+        title: "3D Systems Team",
+        location: "3D Systems Office, Porvorim,",   
+        year: "2020",          
+        img: "assets/gallery_images/3dsystems2.jpg"
+    },
+    {
+        id: 9,
+        title: "3D Systems",
+        location: "3D Systems Office, Porvorim,",   
+        year: "2020",          
+        img: "assets/gallery_images/3dsystems3.jpg"
     },
     {
         id: 10,
@@ -79,7 +121,7 @@ export const galleryData = [
     },
     {
         id: 11,
-        title: "Irix - Organizing Team",
+        title: "Irix - Organizers",
         location: "Chowgule College, Margao",   
         year: "2020",          
         img: "assets/gallery_images/irix1.jpg"
@@ -104,5 +146,19 @@ export const galleryData = [
         location: "Dhempe College, Miramar",   
         year: "2017",          
         img: "assets/gallery_images/technogyaan1.jpg"
+    },
+    {
+        id: 14,
+        title: "Techlipse - Organizers",
+        location: "St. Xavier's College, Mapusa",   
+        year: "2017",          
+        img: "assets/gallery_images/techlipse21.jpg"
+    },
+    {
+        id: 15,
+        title: "Techlipse - Organizers",
+        location: "St. Xavier's College, Mapusa",   
+        year: "2017",          
+        img: "assets/gallery_images/techlipse22.jpg"
     },
 ]
